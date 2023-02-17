529250381b0f85fc452314622647cf84adb2aaf4
OhhOhh7777 <112569602+OhhOhh7777@users.noreply.github.com>
2023-02-17 13:43:34 -0500

    Click
    exploring buttons and functions in vue

---
+++ 02-17.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+    // data, functions 
+    data(){
+        return {
+            title: ' The Final Empire 1',
+            title2: 'The Final Empire 2',
+            author: 'Brandon Sanderson',
+            count: 0
+        }
+    },
+    
+    methods: {
+        increment() {
+          this.count++
+        }
+      }
+
+
+});
+app.mount('#app')
